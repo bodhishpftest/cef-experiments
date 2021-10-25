41088ff33cd16eb4d694d6bc92c4e296cdb2c7da
Bodhish Thomas <bodhish@gmail.com>
2021-10-25 16:51:39 +0530

    Update grading function

--- grade.js
+++ grade.js
@@ -1,12 +1,5 @@
-"use strict";
-const shell = require("shelljs");
-const fetch = require("node-fetch");
-const fs = require("fs");
-const download = require("download");
-
-async function downloadFile(fileName, url) {
-  fs.writeFileSync(fileName, await download(url));
-}
+import fetch from "node-fetch";
+import fs from "fs";
 
 async function submitGrading(url, data) {
   const response = await fetch(url, {
@@ -27,38 +20,29 @@ const readFile = async (filePath) => {
     const data = await fs.promises.readFile(filePath, "utf8");
     return data;
   } catch (err) {
-    console.log(err);
+    return;
   }
 };
 
-const execute = async (event) => {
-  // Parse data from request
-  const data = JSON.parse(event.body);
-  // Read the result
+const execute = async (token, endpoint) => {
   const result = await readFile("/report.json");
 
   let response = {
     results: result,
-    submission: data.submission,
-    token: data.token,
+    token: token,
   };
 
-  let gradingResponse = await submitGrading(data.gradingUrl, response);
+  let gradingResponse = await submitGrading(endpoint, response);
 
-  console.log(gradingResponse);
-};
-
-let data = {
-  body: `{
-    "testScript": "https://raw.githubusercontent.com/pupilfirst/vta-test-scripts/master/wd-101/level_7/todaysEntries.test.js",
-    "userInput": "https://gist.githubusercontent.com/bodhish/00720b97cad7fb266e40d1587f3df209/raw/784158df7cc17383953076e496533b2f3550dbfb/todaysEntries,js",
-    "submission": {"id": "Foo" },
-    "gradingUrl": "https://d11027cf7b0ebc.localhost.run/test/webhook",
-    "token": "random_hash"
-  }`,
+  console.log(`Grading Response: ${gradingResponse?.status}`);
 };
 
 (async () => {
-  // let result = await hello(data);
-  console.log("result");
+  let token = process.env.TOKEN;
+  let endpoint = process.env.ENDPOINT;
+  if (token && endpoint) {
+    execute(process.env.TOKEN, process.env.ENDPOINT);
+  } else {
+    console.log("Please provide token end endpoint");
+  }
 })();
